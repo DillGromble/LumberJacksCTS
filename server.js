cff2165472719ec315914a969438fd7d7477e43e
Josh White <whitejosh42@gmail.com>
2017-06-18 19:04:59 -0500

    update server port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static('public'))
 app.use(express.static(path.join(__dirname, 'node_modules/phaser-ce/build')))
   .get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')))
 
-const server = app.listen(8080, () => console.log('Running on port 8080'))
+const server = app.listen(process.env.PORT || 8080, () => console.log('Running on port 8080'))
 
 const io = socketio(server)
 
